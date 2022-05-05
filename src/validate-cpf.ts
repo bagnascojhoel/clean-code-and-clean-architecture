e12b5a0474e5454a88183fad90cc2ab584e9bc66
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2022-05-04 22:15:52 -0300

    Refact validate cpf function

--- src/validate-cpf.ts
+++ src/validate-cpf.ts
@@ -1,55 +1,51 @@
-// @ts-nocheck
-function hasCpfLength(aString) {
-    console.log(aString.length)
-    return aString.length === 11 ^ aString.length === 14;
+function hasCpfLength(aString: string): boolean {
+    return aString.length === 11;
 }
 
-function isSameCharacter(aString) {
+function isSameCharacter(aString: string) {
     return aString.split("").every(character => character === aString[0])
 }
 
-export function validate(aString) {
-	if (!aString) return false;
-    if (!hasCpfLength(aString)) return false;
+function leaveOnlyDigits(aString: string): string {
+    return aString.replaceAll(/[^0-9]/g, '');
+}
 
-    const unmaskedString = aString
-        .replace('.','')
-        .replace('.','')
-        .replace('-','')
-        .replace(" ","");  
-    
-    if (!isSameCharacter(unmaskedString)) {
-        try{  
-            let d1 = 0, d2 = 0;  
-            let dg1 = 0, dg2 = 0, rest = 0;  
-            let nDigResult;  
-            
-            let digito;  
-            for (let i = 1; i < unmaskedString.length -1; i++) {  
-                digito = parseInt(unmaskedString.substring(i -1, i));  							
-                d1 = d1 + ( 11 - i ) * digito;  
+function calcCpfSummationUntilNinthDigit(aString: string): number {
+    let result: number = 0;
+    for (let i = 0; i <= 8; i++)
+        result += parseInt(aString[i]) * (10 - i);
+    return result;
+}
 
-                d2 = d2 + ( 12 - i ) * digito;  
-            };  
-                
-            rest = (d1 % 11);  
-    
-            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-            d2 += 2 * dg1;  
-            rest = (d2 % 11);  
-            if (rest < 2)  
-                dg2 = 0;  
-            else  
-                dg2 = 11 - rest;  
-    
-                let nDigVerific = unmaskedString.substring(unmaskedString.length-2, unmaskedString.length);  
-            nDigResult = "" + dg1 + "" + dg2;  
-            return nDigVerific == nDigResult;
-        }catch (e){  
-            console.error("Erro !"+e);
+function calculateFirstVerifierDigit(value: string): number {
+    const summationUntilNinthDigit = calcCpfSummationUntilNinthDigit(value);
+    const remainder = summationUntilNinthDigit % 11;
+    return remainder < 2 ? 0 : 11 - remainder;
+}
 
-            return false;  
-        }  
-    } else return false;
+function calcCpfSummationUntilTenthDigit(digits: string[]): number {
+    let result: number = 0;
+    for (let i = 0; i <= 9; i++)
+        result += parseInt(digits[i]) * (11 - i);
+    return result;
+}
 
+function calculateSecondVerifierDigit(
+    baseString: string,
+    firstVerifierDigit: number
+): number {
+    const digits = baseString.split('');
+    digits[9] = firstVerifierDigit.toString();
+    const summationUntilTenthDigit = calcCpfSummationUntilTenthDigit(digits);
+    const remainder = summationUntilTenthDigit % 11;
+    return remainder < 2 ? 0 : 11 - remainder;
+}
+
+export function validateCpf(value: any): boolean {
+    if (!value) return false;
+    const aString = leaveOnlyDigits(value);
+    if (!hasCpfLength(aString) || isSameCharacter(aString)) return false;
+    const firstVerifierDigit = calculateFirstVerifierDigit(aString);
+    const secondVerifierDigit = calculateSecondVerifierDigit(aString, firstVerifierDigit);
+    return parseInt(aString[9]) == firstVerifierDigit && parseInt(aString[10]) == secondVerifierDigit;
 }
--- test/validate-cpf.test.ts
+++ test/validate-cpf.test.ts
@@ -1,60 +1,60 @@
-import { validate } from '../src/validate-cpf'
+import { validateCpf } from '../src/validate-cpf'
 
 describe('Invalid CPF scenarios', () => {
     test('Should be false when value is null', () => {
-        let actual = validate(null);
+        let actual = validateCpf(null);
         expect(actual).toBe(false);
     })
 
     test('Should be false when value is undefined', () => {
-        let actual = validate(undefined);
+        let actual = validateCpf(undefined);
         expect(actual).toBe(false);
     })
 
     test('Should be false when value is empty string', () => {
-        let actual = validate(undefined);
+        let actual = validateCpf(undefined);
         expect(actual).toBe(false);
     })
-    
+
     test('Should be false when value is smaller than 11', () => {
-        const actual = validate('1112223334');
+        const actual = validateCpf('1112223334');
         expect(actual).toBe(false);
     })
-    
+
     test('Should be false when value is smaller greater than 14', () => {
-        const actual = validate('111.222.333.444');
+        const actual = validateCpf('111.222.333.444');
         expect(actual).toBe(false);
     })
-    
+
     test('Should be false when value is the same digit repeated', () => {
-        const actual = validate('11111111111');
+        const actual = validateCpf('11111111111');
         expect(actual).toBe(false);
     })
 
     test('Should be false when first verifier digit is wrong', () => {
-        const actual = validate('11144477789');
+        const actual = validateCpf('11144477789');
         expect(actual).toBe(false);
     })
 
     test('Should be false when second verifier digit is wrong', () => {
-        const actual = validate('11144477733');
+        const actual = validateCpf('11144477733');
         expect(actual).toBe(false);
     })
 })
 
 describe('Valid CPF scenarios', () => {
     test('Should be true when value is unmasked valid CPF', () => {
-        const actual = validate('80700816003');
+        const actual = validateCpf('80700816003');
         expect(actual).toBe(true);
     })
-    
+
     test('Should be true when value is masked valid CPF', () => {
-        const actual = validate('715.130.640-70');
+        const actual = validateCpf('715.130.640-70');
         expect(actual).toBe(true);
     })
 
     test('Should be false when verifier digits are right', () => {
-        const actual = validate('11144477735');
+        const actual = validateCpf('11144477735');
         expect(actual).toBe(true);
     })
 })
